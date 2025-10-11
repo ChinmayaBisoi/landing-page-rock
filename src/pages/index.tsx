641d5949d53fbc918595ced767a514bcf06abe45
ChinmayaBisoi <bisoi.chinmaya.1999@gmail.com>
2025-10-11 13:37:24 +0530

    update: main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import Image from "next/image";
 import Navbar from "~/components/Navbar";
 
 const inter = Inter({
@@ -19,9 +20,22 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className={`${inter.className} font-sans min-h-screen`}>
-      <div className="py-6">
+      <div className="">
         <Navbar />
       </div>
+      <div className="">
+        <Image
+          placeholder="blur"
+          blurDataURL="/assets/hero-placeholder.png"
+          src="/assets/hero.webp"
+          alt="Hero"
+          width={1920}
+          height={1080}
+          className="absolute inset-0 w-full h-full max-h-full object-cover object-bottom"
+          priority
+        />
+      </div>
+      {/* <Hero /> */}
     </div>
   );
 }
